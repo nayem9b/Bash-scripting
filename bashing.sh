a5edae1fdc0c99f2cc541220ed44614aff7b1b2b
nayem9b <razwanniam1@gmail.com>
2024-06-23 00:46:27 +0600

    conditional user permisiion

--- bashing.sh
+++ bashing.sh
@@ -42,12 +42,48 @@
 #     echo "age is smaller"
 # fi
 
-read -p "Enter your score: " score
+# read -p "Enter your score: " score
 
-if [ $score -ge 80 ]; then
-    echo "You got A+"
-elif [[ $score -ge 70 && $score -le 79 ]]; then  #Duita condition && operator use korar karone [[]] dewa lagche
-    echo "You got A"
+# if [ $score -ge 80 ]; then
+#     echo "You got A+"
+# elif [[ $score > 69 || $score < 80 ]]; then  #Duita condition && operator use korar karone [[]] dewa lagche
+#     echo "You got A"
+# else
+#     echo "your score is below than A"
+# fi
+
+#if file exist
+# if [[ -f foo.txt ]]; then
+#     echo "File exist"
+# else
+#     echo "File Does't exist"
+# fi
+
+
+# #if directory exist
+# if [[ -d devopss ]]; then
+#     echo "Directory exist"
+# else
+#     echo "Directory Does't exist"
+# fi
+
+#if directory and file exist
+# if [[ -e devops && -e fool.txt ]]; then  #e means it can be file or directory, anything (explicitly)
+#     echo "Directory exist"
+# else
+#     echo "Directory Does't exist"
+# fi
+
+#if file is empty or not
+# if [[ -e empty-file && -s empty-file ]]; then # -s diye empty check kore
+#     echo "File is not empty"
+# else
+#     echo "File is empty"
+# fi
+
+#if file has read permission
+if [[ -r foo.txt ]]; then # -s diye empty check kore
+    echo "File has read permission"
 else
-    echo "your score is below than A"
+    echo "File has no read permission"
 fi
