ce92ac1812c704f9ff0a0f078a82c642ef6b1167
nayem9b <razwanniam1@gmail.com>
2024-10-11 08:01:36 +0600

    created nginx server with ec2 with terraform

---
+++ Terraform/aws/aws-vpc-ec2-nginx/ec2.tf
@@ -0,0 +1,18 @@
+# EC2 instance For Nginx setup
+resource "aws_instance" "nginxserver" {
+  ami                         = "ami-0fff1b9a61dec8a5f"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public-subnet.id
+  vpc_security_group_ids      = [aws_security_group.nginx-sg.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+            #!/bin/bash
+            sudo yum install nginx -y
+            sudo systemctl start nginx
+            EOF
+
+  tags = {
+    Name = "NginxServer"
+  }
+}
